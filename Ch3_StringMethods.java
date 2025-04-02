51f4d59301a4b220cc645d1c287e6b702430456b
AkashKumarVishwakarma <akashkumargyn2002@gamil.com>
2025-04-02 22:11:12 +0530

    second

---
+++ Ch3_StringMethods.java
@@ -0,0 +1,12 @@
+public class Ch3_StringMethods {
+    public static void main(String[] args) {
+
+        // String a = "Harry";
+        // String a  = new String("Akash");
+
+        String name = "Harry";
+        System.out.println(name);
+        
+        
+    }
+}
--- Ch3_Strings.java
+++ Ch3_Strings.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Ch3_Strings {
     public static void main(String[] args) {
         
-        String name = new String("Akash");
+        // String name = new String("Akash");
         // String hi = "babu";
         // System.out.println(hi);
 
-        System.out.println(name);
+        // System.out.println(name);
+
+        // String name = "Harry";
+        // System.out.print("The name is : ");
+        // System.out.println(name);
+
+        // int a = 6;
+        // float b = 5.6454f;
+        // System.out.printf("The value of a is %d and value of b is %f ", a, b);
+
+        // System.out.printf("The value of a is %d and value of b is %8.2f ", a, b);
+
+        // System.out.format("The value of a is %d and value of b is %f", a, b);
+
+        // Scanner sc = new Scanner(System.in);
+
+        // String st = sc.nextLine();
+        // System.out.println(st);
     }
 }
